b4a452b8b908293a572e82a6137f6dd7d49e96dc
BozaiMartin <bozaimartin@gmail.com>
2025-09-24 09:40:19 +0200

    email kuldes letrehozasa

--- MauiTalanJo/EmailKuld.cs
+++ MauiTalanJo/EmailKuld.cs
@@ -1,27 +1,26 @@
 ﻿using MailKit.Net.Smtp;
 using MimeKit;
-using System.Net.Mail;
-using System.Threading.Tasks;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using MailKit.Security;
 
 public static class EmailKuld
 {
     public static async Task KuldesAsync(string to, string subject, string body)
     {
         var message = new MimeMessage();
-        message.From.Add(new MailboxAddress("Kiolvasó App", "bozaimartin@gmail.com"));
+        message.From.Add(new MailboxAddress("Kiolvasó App", "bozaimartin2@gmail.com"));
         message.To.Add(new MailboxAddress("", to));
         message.Subject = subject;
-
-        message.Body = new TextPart("plain")
-        {
-            Text = body
-        };
+        message.Body = new TextPart("plain") { Text = body };
 
         using var client = new SmtpClient();
-        await client.ConnectAsync("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
 
-        // Gmail esetén: teljes e-mail + alkalmazásjelszó
-        await client.AuthenticateAsync("bozaimartin@gmail.com", "Demonka0801");
+        // 🔑 SSL cert ellenőrzés lazítása
+        client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+
+        await client.ConnectAsync("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
+        await client.AuthenticateAsync("bozaimartin@gmail.com", "rdyt vhaq bepo ikzz");
 
         await client.SendAsync(message);
         await client.DisconnectAsync(true);
